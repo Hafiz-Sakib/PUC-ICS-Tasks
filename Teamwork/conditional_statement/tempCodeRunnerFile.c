b79d23e1194e08f3071f0bcf78f99fbbd350178c
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-07-05 02:07:48 +0600

    PDF CHANGED

--- Teamwork/conditional_statement/tempCodeRunnerFile.c
+++
@@ -1,4 +0,0 @@
-C program to convert temperature from Fahrenheit to Celsius and Celsius to Fahrenheit
-The basic formula to convert Fahrenheit and Celsius to each other.
-Celsius to Fahrenheit: (°C × 9/5) + 32 = °F
-Fahrenheit to Celsius: (°F − 32) x 5/9 = °C
