949bead4b0f16c544f30ec0385827f98e181a941
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-08-15 00:59:45 +0600

    student height problem with height min added

---
+++ ICS Exam Topics With Solution/C1 Exam Question Solution/studentheight2.c
@@ -0,0 +1,23 @@
+/* Write a C Program that takes the height (in cm) of 8 Students in Your Class and Displayes the Tallest Ones */
+#include <stdio.h>
+#include <float.h>
+int main()
+{
+    int n = 8, i;
+    float tallest = FLT_MIN, x;
+    for (i = 1; i <= 8; i++)
+    {
+        scanf("%f", &x);
+        if (x < 0)
+        {
+            printf("Height Can't be negative\n");
+            break;
+        }
+        else if (x > tallest)
+        {
+            tallest = x;
+        }
+    }
+    printf("%f", tallest);
+    return 0;
+}
